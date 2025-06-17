4a4dcab1ba67316bcfb163b4ec7edd2497ea2252
Cristian Sierra <cristiansierra.rap@gmail.com>
2025-06-16 22:35:07 -0400

    feat: logo app and splash art

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 import { GamePage } from './game/game.page';
+import { SplashScreen } from '@capacitor/splash-screen';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,14 @@ import { GamePage } from './game/game.page';
   imports: [IonApp, IonRouterOutlet, GamePage],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    this.showSplash()
+  }
+
+  async showSplash(){
+  await SplashScreen.show({
+    autoHide: true,
+    showDuration: 3000
+  });
+  }
 }
