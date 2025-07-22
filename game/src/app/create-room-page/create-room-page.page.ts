6f464241f56d1cb66073d1eb6324566458593582
Cristian Sierra <cristiansierra.rap@gmail.com>
2025-07-22 00:23:19 -0400

    remove: method unused

--- game/src/app/create-room-page/create-room-page.page.ts
+++ game/src/app/create-room-page/create-room-page.page.ts
@@ -44,34 +44,4 @@ export class CreateRoomPagePage implements OnInit {
       this.websocketsService.sendStartedGameStatus();
       this.router.navigate(['/game'],{queryParams: { turn: 1 }});
     }
-  
-    
-  async openCustomModal() {
-    const modal = await this.modalController.create({
-      component: CustomGameModalComponent,
-      componentProps: { size: 8, mines: 10 },
-      cssClass: 'custom-small-modal',
-      breakpoints: [0, 0.5, 1],  // Puntos de ruptura para hacerlo responsive
-      initialBreakpoint: 0.5,    // Comienza en el 50% de la pantalla
-      backdropDismiss: true,     // Permite cerrar haciendo clic fuera
-      showBackdrop: true,
-    });
-    await modal.present();
-
-    const { data } = await modal.onDidDismiss();
-    if (data) {
-      this.setDifficulty(data.size, data.mines);
-    }
-  }
-
-
-  confirmCustomGame() {
-    const maxMines = this.customSize * this.customSize - 1;
-    if (this.customSize < 2 || this.customMines < 1 || this.customMines > maxMines) {
-      alert(`Valores inválidos. Tamaño mínimo 2 y minas entre 1 y ${maxMines}.`);
-      return;
-    }
-    this.openCustomButton = false;
-    this.setDifficulty(this.customSize, this.customMines);
-  }
 }
